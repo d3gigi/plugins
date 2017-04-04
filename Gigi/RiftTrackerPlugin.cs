c489c1214968f7bf9001db8e46f1258c9e2615dd
d3gigi <d3gigi@gmail.com>
2017-04-04 20:00:27 +0200

    added globe support
    (sorting still fails)

--- Gigi/RiftTrackerPlugin.cs
+++ Gigi/RiftTrackerPlugin.cs
@@ -9,17 +9,35 @@ namespace Turbo.Plugins.Gigi
  
     public class RiftTrackerPlugin : BasePlugin, IInGameTopPainter, IMonsterKilledHandler, INewAreaHandler, IAfterCollectHandler, IKeyEventHandler
     {
+        public TopTableCellDecorator DefaultCellDecorator { get; set; }
+        public TopTableCellDecorator HighlightCellDecorator { get; set; }
+        public uint SortByLine { get; set; }
+        public bool SortDescending { get; set; }
+        public float Table2TableXDistance { get; set; }
+        public float Table2TableYDistance { get; set; }
+        public float XPosRatio { get; set; }
+        public float YPosRatio { get; set; }
+        public string HeaderSingleProgression { get; set; }
+        public string HeaderTrackedProgression { get; set; }
+        public string HeaderKilledProgression { get; set; }
+        public string HeaderTrackedCount { get; set; }
+        public string HeaderKilledCount { get; set; }
+        public bool ShowSingleProgression { get; set; }
+        public bool ShowTrackedProgression { get; set; }
+        public bool ShowKilledProgression { get; set; }
+        public bool ShowTrackedCount { get; set; }
+        public bool ShowKilledCount { get; set; }
+        public bool IncludeProgressionOrbs { get; set; }
+        public float CellRatioHeight { get; set; }
+        public IKeyEvent tKey { get; set; }        
         private Dictionary<string, HashSet<uint>> MonsterTracked = new Dictionary<string, HashSet<uint>>();
 		private Dictionary<string, Dictionary<string, float>> MonsterProgression = new Dictionary<string, Dictionary<string, float>>();
 		private Dictionary<string, Dictionary<string, int>> MonsterSeenCount = new Dictionary<string, Dictionary<string, int>>();
         private Dictionary<string, Dictionary<string, int>> MonsterSummonedCount = new Dictionary<string, Dictionary<string, int>>();
 		private Dictionary<string, Dictionary<string, int>> MonsterKilledCount = new Dictionary<string, Dictionary<string, int>>();
-        public TopTableCellDecorator DefaultCellDecorator { get; set; }
-        public TopTableCellDecorator HighlightCellDecorator { get; set; }
-        public IKeyEvent tKey;
         private List<TopTable> Tables = new List<TopTable>();
         private string currentFloor = "";
-        private bool Show = true;
+        private bool Show = false;
         private bool tablesProcessed = false;
         private HorizontalAlign align = Default.HorizontalAlign.Center;
         //shameless copy from https://github.com/JackCeparou/JackCeparouCompass/blob/master/RiftInfoPlugin.cs
@@ -63,7 +81,29 @@ namespace Turbo.Plugins.Gigi
         public override void Load(IController hud)
         {
             base.Load(hud);
+            //Table Definition
+            HeaderSingleProgression = "%S";
+            HeaderTrackedProgression = "%T";
+            HeaderTrackedCount = "#T";
+            HeaderKilledProgression = "%K";
+            HeaderKilledCount = "#K";
+            ShowSingleProgression = true;
+            ShowTrackedProgression = true;
+            ShowTrackedCount = true;
+            ShowKilledProgression = true;
+            ShowKilledCount = true;
+            IncludeProgressionOrbs = true;
+            SortByLine = 4;
+            SortDescending = true;
+            //Spacing
+            Table2TableXDistance = 0.18f;
+            Table2TableYDistance = 0.05f;
+            XPosRatio = 0.1f;
+            YPosRatio = 0.075f;
+            CellRatioHeight  = 22 / (float)Hud.Window.Size.Height;
+            //Display Definition
             tKey = Hud.Input.CreateKeyEvent(true, Key.F7, false, false, false);
+            //Colorization
             DefaultCellDecorator = new TopTableCellDecorator(Hud)
             {
                 BackgroundBrush = Hud.Render.CreateBrush(185, 75, 75, 75, 0),
@@ -79,6 +119,11 @@ namespace Turbo.Plugins.Gigi
         }		
 
         public void OnNewArea(bool isNewGame, ISnoArea area){
+            if (isNewGame){
+                ClearData();
+                Show = false;
+                tablesProcessed = false;
+            }
             if (!Hud.Game.IsInTown){
                 currentFloor = area.NameLocalized;
                 if (!MonsterProgression.ContainsKey(currentFloor) && 
@@ -149,6 +194,7 @@ namespace Turbo.Plugins.Gigi
                 foreach(var m in MonsterTracked)
                     if (m.Value != null)
                         m.Value.Clear();
+                MonsterTracked.Clear();
             }
             if (Tables != null){
                 foreach(var t in Tables){
@@ -195,16 +241,117 @@ namespace Turbo.Plugins.Gigi
 
             //add progression entry for monster
             if (!MonsterProgression[floor].ContainsKey(m.SnoMonster.NameLocalized)){
-                MonsterProgression[floor].Add(m.SnoMonster.NameLocalized, 
-                m.SnoMonster.RiftProgression);
+                MonsterProgression[floor].Add(m.SnoMonster.NameLocalized, m.SnoMonster.RiftProgression);
             }
 		}
 
-        private TopTable getNewTable(float XPosRatio, float YPosRatio, string f){
+        private void ProcessGlobe(IActor globe, string floor){ 
+            //handle a globe as if it would be a monster (same datastructure)
+            //make sure data accessing is safe (we probably don't need this - better be safe than sorry)
+            if (!MonsterProgression.ContainsKey(floor) || 
+                !MonsterSeenCount.ContainsKey(floor) ||
+                !MonsterSummonedCount.ContainsKey(floor) ||
+                !MonsterTracked.ContainsKey(floor))
+                return;
+            
+			//do we already know that globe?
+            if (MonsterTracked[floor].Contains(globe.AnnId))
+                return;
+            MonsterTracked[floor].Add(globe.AnnId);            
+            
+            //track it as seen
+            if (MonsterSeenCount[floor].ContainsKey(globe.SnoActor.Kind.ToString("G")))
+                MonsterSeenCount[floor][globe.SnoActor.Kind.ToString("G")] += 1;
+            else
+                MonsterSeenCount[floor].Add(globe.SnoActor.Kind.ToString("G"), 1);
+
+            //track as killed also (there is no "OnPickUp" event for globes)
+            if (MonsterKilledCount[currentFloor].ContainsKey(globe.SnoActor.Kind.ToString("G")))
+                MonsterKilledCount[currentFloor][globe.SnoActor.Kind.ToString("G")] += 1;
+            else
+                MonsterKilledCount[currentFloor].Add(globe.SnoActor.Kind.ToString("G"), 1);
+
+            //add progression entry for monster
+            if (!MonsterProgression[floor].ContainsKey(globe.SnoActor.Kind.ToString("G"))){
+                MonsterProgression[floor].Add(globe.SnoActor.Kind.ToString("G"), 7.5f);
+            }
+
+        }
+
+        private IEnumerable<TopTableHeader> getHeader(string f){
+            yield return new TopTableHeader(Hud, (pos, curPos) => "Type @ " + f)        //Monstertype
+                {
+                    RatioHeight = CellRatioHeight, // define only once on first column, value on others will be ignored
+                    RatioWidth = 108 / (float)Hud.Window.Size.Height,
+                    HighlightFunc = (pos, curPos) => false,
+                    TextAlign = align,
+                };
+            if (ShowSingleProgression){
+                yield return new TopTableHeader(Hud, (pos, curPos) => HeaderSingleProgression)    //Percent Progression of Monstertype
+                    {
+                        RatioWidth = 0.04f,
+                        HighlightFunc = (pos, curPos) => false,
+                        TextAlign = align,
+                    };
+            }
+            if (ShowTrackedProgression){
+                yield return new TopTableHeader(Hud, (pos, curPos) => HeaderTrackedProgression)   //Percent Progression of all Monstertype instances
+                    {
+                        RatioWidth = 0.05f,
+                        HighlightFunc = (pos, curPos) => false,
+                        TextAlign = align,
+                    };
+            }
+            if (ShowTrackedCount){
+                yield return new TopTableHeader(Hud, (pos, curPos) => HeaderTrackedCount)   //Count of all Monstertype instances
+                    {
+                        RatioWidth = 0.035f,
+                        HighlightFunc = (pos, curPos) => false,
+                        TextAlign = align,
+                    };
+            }
+            if (ShowKilledProgression){
+                yield return new TopTableHeader(Hud, (pos, curPos) => HeaderKilledProgression)    //Percent Progression off killed Monstertype instances
+                    {
+                        RatioWidth = 0.05f,
+                        HighlightFunc = (pos, curPos) => false,
+                        TextAlign = align,
+                    };
+            }
+            if (ShowKilledCount){
+                yield return new TopTableHeader(Hud, (pos, curPos) => HeaderKilledCount)    //Count of killed Monstertype instances
+                    {
+                        RatioWidth = 0.035f,
+                        HighlightFunc = (pos, curPos) => false,
+                        TextAlign = align,
+                    };
+            }
+        }
+
+        private IEnumerable<TopTableCell> getLine(string floor, string mtype){
+            yield return new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => mtype);
+            if (ShowSingleProgression){
+                yield return new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getSingleProgress(floor, mtype));
+            }
+            if (ShowTrackedProgression){
+                yield return new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getTrackedProgress(floor, mtype));
+            }
+            if (ShowTrackedCount){
+                yield return new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getTrackedCount(floor, mtype));
+            }
+            if (ShowKilledProgression){
+                yield return new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getKilledProgression(floor, mtype));
+            }
+            if (ShowKilledCount){
+                yield return new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getKilledCount(floor, mtype));
+            }
+        }
+
+        private TopTable getNewTable(float XRatio, float YRatio, string f){
             TopTable Table = new TopTable(Hud)
             {
-                RatioPositionX = XPosRatio,
-                RatioPositionY = YPosRatio,
+                RatioPositionX = XRatio,
+                RatioPositionY = YRatio,
                 HorizontalCenter = true,
                 VerticalCenter = false,
                 PositionFromRight = false,
@@ -220,66 +367,20 @@ namespace Turbo.Plugins.Gigi
                     TextFont = Hud.Render.CreateFont("tahoma", 6.5f, 255, 255, 255, 255, false, false, true),
                 }
             };
-
-            Table.DefineColumns(
-                new TopTableHeader(Hud, (pos, curPos) => "Type on " + f)        //Monstertype
-                {
-                    RatioHeight = 22 / 1080f, // define only once on first column, value on others will be ignored
-                    RatioWidth = 108 / 1080f,
-                    HighlightFunc = (pos, curPos) => false,
-                    TextAlign = align,
-                },
-                new TopTableHeader(Hud, (pos, curPos) => "%S")    //Percent Progression of Monstertype
-                {
-                    RatioWidth = 0.04f,
-                    HighlightFunc = (pos, curPos) => false,
-                    TextAlign = align,
-                },
-                new TopTableHeader(Hud, (pos, curPos) => "%T")   //Percent Progression of all Monstertype instances
-                {
-                    RatioWidth = 0.05f,
-                    HighlightFunc = (pos, curPos) => false,
-                    TextAlign = align,
-                },
-                new TopTableHeader(Hud, (pos, curPos) => "#T")   //Count of all Monstertype instances
-                {
-                    RatioWidth = 0.035f,
-                    HighlightFunc = (pos, curPos) => false,
-                    TextAlign = align,
-                },
-                new TopTableHeader(Hud, (pos, curPos) => "%K")    //Percent Progression off killed Monstertype instances
-                {
-                    RatioWidth = 0.05f,
-                    HighlightFunc = (pos, curPos) => false,
-                    TextAlign = align,
-                },
-                new TopTableHeader(Hud, (pos, curPos) => "#K")    //Count of killed Monstertype instances
-                {
-                    RatioWidth = 0.035f,
-                    HighlightFunc = (pos, curPos) => false,
-                    TextAlign = align,
-                }
-            );
+            Table.DefineColumns(getHeader(f).ToArray());
             return Table;      
         }
 
-        private void DrawTables(){
-            if (!tablesProcessed)
-                processTables();
-            foreach(TopTable t in Tables)
-                t.Paint();
-        }
-
-        private void processTables(){
+        private void ProcessTables(){
             //scaling and positining
             var w = Hud.Window.Size.Width;
             var h = Hud.Window.Size.Height;
-            var xoff = 0.05f;
-            var yoff = 0.05f;
-            var XPosRatio = 0.1f;
-            var YPosRatio = 0.05f;
-            var count = 0;
+            var XRatio = XPosRatio;
+            var YRatio = YPosRatio;
+            int tablesPerLine = (int)(1.0/Table2TableXDistance);
+            int count = 1;
             //iterate floors where monsters are tracked for
+            int maxLineCount = 0;
             foreach(string f in MonsterTracked.Keys.ToList()){
                 if (!MonsterProgression.ContainsKey(f) || 
                     !MonsterSeenCount.ContainsKey(f) ||
@@ -287,35 +388,35 @@ namespace Turbo.Plugins.Gigi
                     !MonsterKilledCount.ContainsKey(f))
                     continue;
                 //create table for floor
-                if (count == 5){
-                    XPosRatio = 0.1f;
-                    YPosRatio = 0.55f;
-                }
-                TopTable t = getNewTable(XPosRatio, YPosRatio, f);
+                TopTable t = getNewTable(XRatio, YRatio, f);
                 //fill table with each tracked monster on floor
                 foreach(string m in MonsterProgression[f].Keys.ToList()){
                     string mtype = m.ToString();
                     string floor = f.ToString();
+                    TopTableCell[] l = getLine(floor, mtype).ToArray();
                     t.AddLine(
                         new TopTableHeader(Hud, (pos, curPos) => string.Empty)
                         {
-                            RatioWidth = 62 / 1080f, // define only once on first line, value on other will be ignored
-                            RatioHeight = 22 / 1080f,
+                            RatioWidth = 62 / (float)Hud.Window.Size.Height,
+                            RatioHeight = CellRatioHeight,
                             HighlightFunc = (pos, curPos) => false, //highlight empty line for new area
                             TextAlign = align,
-                        },
-                        new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => mtype),
-                        new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getSingleProgress(floor, mtype)),
-                        new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getTrackedProgress(floor, mtype)),
-                        new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getTrackedCount(floor, mtype)),
-                        new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getKilledProgression(floor, mtype)),
-                        new TopTableCell(Hud, (line, column, lineSorted, columnSorted) => getKilledCount(floor, mtype))
+                        }, l
                     );
-                }//foreach-end monster iteration per floor
-                XPosRatio += 0.18f;
+                }//foreach-end monster 
+                //sort and add table
+                t.SortLines((int)SortByLine, SortDescending);
                 Tables.Add(t);
-                count++;               
-            }
+                //counters and offsets
+                maxLineCount = (t.Lines.Count > maxLineCount) ? t.Lines.Count : maxLineCount;
+                XRatio += Table2TableXDistance;       
+                if (count % tablesPerLine == 0){    //move tables to next line
+                    XRatio = XPosRatio;
+                    YRatio = YPosRatio + CellRatioHeight * maxLineCount + Table2TableYDistance;
+                    maxLineCount = 0;
+                }
+                count++;        
+            }//foreach-end floor 
             tablesProcessed = true;
         }
 
@@ -362,6 +463,13 @@ namespace Turbo.Plugins.Gigi
             return killed.ToString();
         }
 
+        private void DrawTables(){
+            if (!tablesProcessed)
+                ProcessTables();
+            foreach(TopTable t in Tables)
+                t.Paint();
+        }
+
         public void PaintTopInGame(ClipState clipState)
         {
             if (clipState != ClipState.BeforeClip) return;
@@ -375,14 +483,21 @@ namespace Turbo.Plugins.Gigi
             if (Hud.Game.SpecialArea != SpecialArea.Rift && Hud.Game.SpecialArea != SpecialArea.GreaterRift){
                 return; //not in a rift
             }
+            
+            //collect data
             var monsters = Hud.Game.AliveMonsters.Where(m => !m.IsElite);
             var floor = currentFloor;
-          	foreach (var monster in monsters)
+          	foreach (IMonster monster in monsters)
 				ProcessMonster(monster, floor);
+            if (IncludeProgressionOrbs){
+                foreach(IActor globe in Hud.Game.Actors.Where(a => a.SnoActor.Kind == ActorKind.RiftOrb))
+                    ProcessGlobe(globe, floor);
+            }
+
             tablesProcessed = false;
             Show = true;
         }
- 
+
     }
 
 
